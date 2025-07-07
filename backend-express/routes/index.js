1ecd64f3cf4d9415553674c815b95799d4150f6c
afrandormd <afrandormd@gmail.com>
2025-07-07 17:28:54 +0700

    feat: membuat service tampil data user

--- backend-express/routes/index.js
+++ backend-express/routes/index.js
@@ -4,11 +4,17 @@ import express from "express";
 // init express router
 const router = express.Router();
 
+// import verifyToken
+import verifyToken from "../middlewares/auth.js";
+
 // import register controller
 import RegisterController from "../controllers/RegisterController.js";
 
-// import register controller
+// import login controller
 import LoginController from "../controllers/LoginController.js";
+
+// import user controller
+import UserController from "../controllers/UserController.js";
 
 // import validate register
 import { validateRegister, validateLogin } from "../utils/validators/auth.js";
@@ -18,6 +24,9 @@ router.post("/register", validateRegister, RegisterController);
 
 // define route for login
 router.post("/login", validateLogin, LoginController);
+
+// define route for user
+router.get("/admin/users", verifyToken, UserController);
 
 // export router
 export default router;
